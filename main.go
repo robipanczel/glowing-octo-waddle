28bbc49b7f69fcc6e9481395be690e98aa7ac980
Róbert Pánczél <robi.panczel@gmail.com>
2023-08-11 01:26:16 +0200

    Basic gin

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "pong")
+	})
+
+	anime := r.Group("/hello")
+	{
+		anime.GET("", func(ctx *gin.Context) {
+			ctx.IndentedJSON(http.StatusOK, "HELLO WORLD")
+		})
+		anime.POST("", func(ctx *gin.Context) {
+			ctx.IndentedJSON(http.StatusOK, "HELLO WORLD")
+		})
+		anime.DELETE("", func(ctx *gin.Context) {
+			ctx.IndentedJSON(http.StatusOK, "HELLO WORLD")
+		})
+	}
+
+	return r
+}
 
 func main() {
-	fmt.Printf("Hello world")
+	r := setupRouter()
+
+	r.Run(":8080")
 }
